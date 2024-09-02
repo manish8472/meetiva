d9a916a6010a11d642a8049edb55cfe2d7fb1c3a
Manish Kumar <91687469+manish8472@users.noreply.github.com>
2024-09-02 22:03:17 +0530

    Delete assets directory

--- assets/DarkTheme-CIxXSPf4.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-ORadBm-k.js";function e(){return r.jsx("div",{})}export{e as default};
--- assets/LightTheme-CTOQf7Oa.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-ORadBm-k.js";function r(){return t.jsx("div",{})}export{r as default};
