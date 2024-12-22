d0cea2fcce514a9824e8ef45f8aee8eca2392d99
Manish Kumar <91687469+manish8472@users.noreply.github.com>
2024-12-22 09:35:06 +0530

    Delete assets directory

--- assets/DarkTheme-jxD560tU.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-BH0H9GqX.js";function e(){return r.jsx("div",{})}export{e as default};
--- assets/LightTheme-C8zFGibQ.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-BH0H9GqX.js";function r(){return t.jsx("div",{})}export{r as default};
