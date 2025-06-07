167a5c7d4e0ab048ec3fa7a48465b88d33d4cdf4
Manish Kumar <91687469+manish8472@users.noreply.github.com>
2025-06-07 19:51:30 +0530

    Delete assets directory

--- assets/DarkTheme-DkjcNEiw.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-Zs_UPjSc.js";function e(){return r.jsx("div",{})}export{e as default};
--- assets/LightTheme-Cx67C9eH.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-Zs_UPjSc.js";function r(){return t.jsx("div",{})}export{r as default};
